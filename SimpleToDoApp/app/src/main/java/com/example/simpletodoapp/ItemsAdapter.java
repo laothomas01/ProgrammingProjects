36d2a57851896d8d0f1845810d239f548d91feb2
laothomas01 <53920510+laothomas01@users.noreply.github.com>
2020-12-18 13:19:34 -0800

    StretchGoals
    -can edit todo item
    -can update todo item

--- SimpleToDoApp/app/src/main/java/com/example/simpletodoapp/ItemsAdapter.java
+++ SimpleToDoApp/app/src/main/java/com/example/simpletodoapp/ItemsAdapter.java
@@ -1,74 +1,89 @@
-package com.example.simpletodoapp;
+    package com.example.simpletodoapp;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+    import android.content.DialogInterface;
+    import android.view.LayoutInflater;
+    import android.view.View;
+    import android.view.ViewGroup;
+    import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
+    import androidx.annotation.NonNull;
+    import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.List;
+    import java.util.List;
 
-//reponsible for taking data at a particular position and putting it in a view holder.
-//reponsible for displaying data from the model into a row in the recycler view
-public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder> {
-    public interface OnLongClickListener {
-        void onItemLongClicked(int position);
-    }
-    List<String> items;
-    OnLongClickListener longClickListener;
-    public ItemsAdapter(List<String> items,OnLongClickListener longClickListener)
-    {
-        this.items = items;
-        this.longClickListener = longClickListener;
-
-    }
-    @NonNull
-    @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        //Use layout inflate to inflate a view
-        View todoView = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1,parent,false);
-        //wrap it inside a View Holder and return it
-        return new ViewHolder(todoView);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        //Grab the item at the position
-        String item = items.get(position);
-        // Bind the item into the specified view holder
-        holder.bind(item);
-
-    }
-    //tells recycler view how many items are in the list
-    @Override
-    public int getItemCount() {
-        return items.size();
-    }
-
-    //Container to provide easy access to views that represent each row of the list
-    class ViewHolder extends RecyclerView.ViewHolder{
-
-        TextView tvItem;
-        public ViewHolder(@NonNull View itemView)
+    //responsible for taking data at a particular position and putting it in a view holder.
+    //responsible for displaying data from the model into a row in the recycler view
+    public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder> {
+        public interface OnClickListener
         {
-            super(itemView);
-            tvItem = itemView.findViewById(android.R.id.text1);
+            void onItemClicked(int position);
         }
-        //Update the view inside of the view holder with this data.
-        public void bind(String item) {
-            tvItem.setText(item);
-            tvItem.setOnLongClickListener(new View.OnLongClickListener(){
+        public interface OnLongClickListener {
+            void onItemLongClicked(int position);
+        }
 
-                @Override
-                public boolean onLongClick(View v) {
-                  //Notify the listener which position was long pressed
-                    longClickListener.onItemLongClicked(getAdapterPosition());
-                    return true;
-                }
-            });
+        List<String> items;
+        OnLongClickListener longClickListener;
+        OnClickListener clickListener;
+
+        public ItemsAdapter(List<String> items,OnLongClickListener longClickListener, OnClickListener clickListener)
+        {
+            this.items = items;
+            this.longClickListener = longClickListener;
+            this.clickListener = clickListener;
+
+        }
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            //Use layout inflate to inflate a view
+            View todoView = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1,parent,false);
+            //wrap it inside a View Holder and return it
+            return new ViewHolder(todoView);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            //Grab the item at the position
+            String item = items.get(position);
+            // Bind the item into the specified view holder
+            holder.bind(item);
+
+        }
+        //tells recycler view how many items are in the list
+        @Override
+        public int getItemCount() {
+            return items.size();
+        }
+
+        //Container to provide easy access to views that represent each row of the list
+        class ViewHolder extends RecyclerView.ViewHolder{
+
+            TextView tvItem;
+            public ViewHolder(@NonNull View itemView)
+            {
+                super(itemView);
+                tvItem = itemView.findViewById(android.R.id.text1);
+            }
+            //Update the view inside of the view holder with this data.
+            public void bind(String item) {
+                tvItem.setText(item);
+                tvItem.setOnClickListener(new View.OnClickListener(){
+                    public void onClick(View v)
+                    {
+                            clickListener.onItemClicked(getAdapterPosition());
+                    }
+                });
+                tvItem.setOnLongClickListener(new View.OnLongClickListener(){
+
+                    @Override
+                    public boolean onLongClick(View v) {
+                      //Notify the listener which position was long pressed
+                        longClickListener.onItemLongClicked(getAdapterPosition());
+                        return true;
+                    }
+                });
+            }
         }
     }
-}
 
